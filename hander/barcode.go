6b6d614110866bd1ff2055ff1f218bdbbdf37579
bigrocs <bigrocs@qq.com>
2019-08-09 17:18:19 +0800

    自动提交 git 代码

--- hander/barcode.go
+++ hander/barcode.go
@@ -3,8 +3,10 @@ package hander
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 
 	client "github.com/gomsa/tools/k8s/client"
+	"github.com/micro/go-micro/util/log"
 
 	pb "github.com/gomsa/goods-api/proto/barcode"
 	"github.com/gomsa/goods-api/providers/redis"
@@ -17,12 +19,21 @@ type Barcode struct {
 
 // Get 获取商品信息
 func (srv *Barcode) Get(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
+	// 防止空指针报错
+	if req.Goods == nil {
+		return fmt.Errorf("请求参数错误")
+	}
 	label := "barcode:" + req.Goods.Barcode
 	redis := redis.NewClient()
-	if r, err := redis.Get(label).Result(); err != nil && r == "" {
+	log.Log(label)
+	log.Log(req)
+	if r, err := redis.Get(label).Result(); err != nil && r != "" {
+		log.Log(r)
+		log.Log(err)
 		json.Unmarshal([]byte(r), res)
 	} else {
 		err = client.Call(ctx, srv.ServiceName, "Barcode.Get", req, res)
+		log.Log(res, err)
 		redis.SetNX(label, res, 0)
 	}
 	return err
