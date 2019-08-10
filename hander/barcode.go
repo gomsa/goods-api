3d38ea46c8c74ee93d015bf403681fad2e4fc4d8
bigrocs <bigrocs@qq.com>
2019-08-10 15:26:59 +0800

    自动提交 git 代码

--- hander/barcode.go
+++ hander/barcode.go
@@ -26,12 +26,17 @@ func (srv *Barcode) Get(ctx context.Context, req *pb.Request, res *pb.Response) 
 	redis := redis.NewClient()
 	// 报错时 没有数据时 强制更新缓存时 远程读取数据写入 redis
 	if r, err := redis.Get(label).Result(); err == nil && r != "" && !req.Cache {
-		json.Unmarshal([]byte(r), res)
+		err = json.Unmarshal([]byte(r), res)
+		if err != nil {
+			return err
+		}
 	} else {
+		// 远程获取数据
 		err = client.Call(ctx, srv.ServiceName, "Barcodes.Get", req, res)
 		if err != nil {
 			return err
 		}
+		// 数据写入 redis
 		j, _ := json.Marshal(res)
 		err = redis.SetNX(label, j, 0).Err()
 		if err != nil {
